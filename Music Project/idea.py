d7a9ab4ec462291ad67b62ed5b58644f26039a77
Sungwon Son <sungwons@uci.edu>
2019-02-01 22:49:35 +0100

    hi
    hi

---
+++ Music Project/idea.py
@@ -0,0 +1,39 @@
+import os
+
+def menu():
+    current_albums = start_up()
+    print("**********************************")
+    input1 = input("1. Add a album!")
+    input2 = input("2. Remove a album!")
+    input3 = input("3. Change an album status!")
+    input4 = input("4. Get a recommendation!")
+    input5 = input("5. Look at your most played music!")
+    input6 = input("6. Look for upcoming musics by your artists!")
+    print("**********************************")
+    if input1 = True:
+        add_album()
+
+def start_up():
+    
+
+def add_album():
+    pass
+
+def remove_album():
+    pass
+
+def change_album():
+    pass
+
+def get_recommendations():
+    pass
+
+def most_played():
+    pass
+
+def upcoming_music():
+    pass
+    
+if __name__ == "__main__":
+    menu()
+    
--- Music Project/main.py
+++ Music Project/main.py
@@ -1,26 +1,12 @@
 import sys
 import spotipy
 import spotipy.util as util
-'''
-scope = 'user-library-read'
-username = '22jk5kjdeaqulvfeky5y2nxpa'
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print ("Usage: %s username" % (sys.argv[0],))
-    sys.exit()
-token = util.prompt_for_user_token(username, scope)
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print (track['name'] + ' - ' + track['artists'][0]['name'])
-else:
-    print ("Can't get token for", username)
-'''
-
+username='22jk5kjdeaqulvfeky5y2nxpa'
+scope='user-library-read'
+client_id='40f479d045e74be1bbf1baf5da1a5dc8'
+client_secret='e88bd4ecc08c4bb080ba87ae23bf01c1'
+redirect_uri='http://localhost/'
 
 token=util.prompt_for_user_token(username,scope,client_id,client_secret,redirect_uri)
 
